9c407708c53d13aa87d268efe15847f96bf4ceee
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 14:22:10 +0400

    Finished lesson #7 - All About Redux Reducers

---
+++ client/reducers/comments.js
@@ -0,0 +1,11 @@
+// A reducer takes in two things:
+
+// 1. The action (info about what happened)
+// 2. A copy of current state
+
+function comments(state = [], action) {
+    console.log(state, action);
+    return state;
+}
+
+export default comments;

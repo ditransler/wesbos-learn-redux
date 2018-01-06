f1b3160c4a7d15a3329e1163f8b5ffd36b165c1d
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 14:28:04 +0400

    Finished lesson #8 - Integrating our Store with React Router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 // Import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 // Import CSS
 import CSS from './styles/style.styl';
@@ -12,12 +14,14 @@ import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
 );
 
 render(router, document.querySelector('#root'));

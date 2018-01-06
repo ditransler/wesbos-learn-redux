4af945b9b843c52de9f1241ba735d89ff8c177ba
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 20:45:10 +0400

    Finished lesson #13 - Displaying the Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Comments extends Component {
+    render() {
+        return (
+            <div className="comment">
+                I'm the comments!!!
+            </div>
+        );
+    }
+}
+
+export default Comments;
+
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 class Single extends Component {
     render() {
+        // Index of the post
+        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        // Get us the post
+        const post = this.props.posts[i];
         return (
             <div className="single-photo">
-                I'm the single
+                <Photo i={i} post={post} {...this.props} />
+                <Comments />
             </div>
         );
     }

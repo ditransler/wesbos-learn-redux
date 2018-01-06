95b18ed100e4bc78cddfcb2381b2df531312e446
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 21:05:06 +0400

    Finished lesson #15 - Updating Comment State in our Store

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class Comments extends Component {
+    constructor() {
+        super();
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
     renderComment(comment, i) {
         <div className="comment">
             <p>
@@ -11,11 +16,19 @@ class Comments extends Component {
         </div>
     }
 
+    handleSubmit(evt) {
+        evt.preventDefault();
+        const { postId } = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+    }
+
     render() {
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
                     <input ref="author" type="text" placeholder="author" />
                     <input ref="comment" type="text" placeholder="comment" />
                     <input type="submit" hidden />
--- client/components/Single.js
+++ client/components/Single.js
@@ -14,7 +14,7 @@ class Single extends Component {
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments postComments={postComments} />
+                <Comments postComments={postComments} {...this.props} />
             </div>
         );
     }

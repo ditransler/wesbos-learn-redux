0bb0229d648bcea2f7e4f584c1be00e21c84e1c5
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 11:36:13 +0400

    Finished lesson #2 - App Layout + Component Setup

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Reduxstagram</h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,10 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import CSS
+import CSS from './styles/style.styl';
+
+import Main from './components/Main'
+
+render(<Main />, document.querySelector('#root'));

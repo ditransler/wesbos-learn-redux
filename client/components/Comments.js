00f41eab756821d32550ea767223bd4f05050307
Dmitriy Beglov <ditransler@gmail.com>
2018-01-06 20:56:11 +0400

    Finished lesson #14 - Displaying and Adding Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class Comments extends Component {
+    renderComment(comment, i) {
+        <div className="comment">
+            <p>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment" type="button">&times;</button>
+            </p>
+        </div>
+    }
+
     render() {
         return (
-            <div className="comment">
-                I'm the comments!!!
+            <div className="comments">
+                {this.props.postComments.map(this.renderComment)}
+                <form ref="commentForm" className="comment-form">
+                    <input ref="author" type="text" placeholder="author" />
+                    <input ref="comment" type="text" placeholder="comment" />
+                    <input type="submit" hidden />
+                </form>
             </div>
         );
     }
--- client/components/Single.js
+++ client/components/Single.js
@@ -4,14 +4,17 @@ import Comments from './Comments';
 
 class Single extends Component {
     render() {
+        const {postId} = this.props.params;
         // Index of the post
-        const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+        const i = this.props.posts.findIndex((post) => post.code === postId);
         // Get us the post
         const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || [];
+
         return (
             <div className="single-photo">
                 <Photo i={i} post={post} {...this.props} />
-                <Comments />
+                <Comments postComments={postComments} />
             </div>
         );
     }
